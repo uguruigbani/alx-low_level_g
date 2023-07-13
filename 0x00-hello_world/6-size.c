71a4225ebf847c55c689865b8fb31e83461faf22
uguruigbani <uguruigbani@gmail.com>
2023-07-13 16:59:32 -0700

    "Size of File"

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(char)); 
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
+printf("Size of double: %d byte(s)\n", sizeof(double));
+
+return (0);
+}
