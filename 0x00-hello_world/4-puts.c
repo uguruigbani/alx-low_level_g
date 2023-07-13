0b8727546ac1f79d8196500f820d03b4897b1749
uguruigbani <uguruigbani@gmail.com>
2023-07-13 16:26:03 -0700

    "Programming is like building a multilingual puzzle"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
