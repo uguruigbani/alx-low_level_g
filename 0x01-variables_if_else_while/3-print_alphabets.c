7af1d537bac24068e625f9c069efb697abf934b4
uguruigbani <uguruigbani@gmail.com>
2023-07-14 20:02:45 -0700

    ALPHAbet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+        char j;
+	char i;
+
+        j = 'a';
+	i = 'A';
+        while
+                (j <= 'z') {
+                        putchar(j);
+                        j++;
+                }
+	while
+                (i <= 'Z') {
+                        putchar(i);
+                        i++;
+                }
+        putchar('\n');
+        return (0);
+}
