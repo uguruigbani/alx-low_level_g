e0bfb3d35ee1864ec5a6d091b6b155fe61f68f85
uguruigbani <uguruigbani@gmail.com>
2023-07-14 19:54:00 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char j;
+
+	j = 'a';
+	while
+		(j <= 'z'){
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}
