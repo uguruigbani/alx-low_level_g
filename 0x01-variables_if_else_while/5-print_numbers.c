e2019aac7d22bf1bc0a8ee43e6083f3b9df0600c
uguruigbani <uguruigbani@gmail.com>
2023-07-17 01:50:13 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+		printf("%d", j);
+	printf("\n");
+	return (0);
+}
