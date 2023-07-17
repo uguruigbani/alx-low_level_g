b8efc11acbdea3dc56d0c2cb5b7d87e9d7314e2a
uguruigbani <uguruigbani@gmail.com>
2023-07-17 04:34:19 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z' ; j >= 'a' ; j--)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
