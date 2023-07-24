20c36975b5a487a3b81fa1cd502b084d330fb088
uguru igbani <uguruigbani@gmail.com>
2023-07-24 12:07:31 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: Destination value
+ * @src: source value
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[i];
+	}
+	dest[j++] = '\0';
+	return (dest);
+}
