1d4ce8340bd35b0f77ab4ae8ce54cc56b19b7638
uguru igbani <uguruigbani@gmail.com>
2023-07-27 04:07:02 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concantenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strcat(char *dest, char *src);
+{
+
+	int j;
+	int i;
+
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+
+	dest[j] = '\0';
+	return (dest);
+}
