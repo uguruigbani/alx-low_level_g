97713bc0eb99bd388256f270283d058cffc5e9b6
uguru igbani <uguruigbani@gmail.com>
2023-07-27 07:29:38 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,15 +5,13 @@
  * @str: string to be capitalized
  * Return: a pointer to the changed string.
  */
-
 char *cap_string(char *str)
 {
-
 	int index = 0;
 
 	while (str[index])
 	{
-		while ((str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 		index++;
 
 		if (str[index - 1] == ' ' ||
