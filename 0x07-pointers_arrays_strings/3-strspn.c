c5a98a128f2b11ee76fa5bf54a107084138ebb74
uguru igbani <uguruigbani@gmail.com>
2023-07-31 11:48:48 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: input value
+ * @accept: input value
+ * Return: Always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int j, n, value, check;
+
+	value = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[n])
+			{
+				value++;
+				check = 1;
+			}
+		}
+		if (check == 0)
+			return (value);
+	}
+
+	return (value);
+}
