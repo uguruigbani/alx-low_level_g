8b65d99f5787a3439aca35666167af7370d24e0d
uguru igbani <uguruigbani@gmail.com>
2023-08-04 08:41:36 -0700

    percursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,14 @@
 
 /**
  * _print_rev_recursion - function that prints a string in reverse
- * 
- * @s - input value
+ *
+ * @s: - input value
  *
  * Return: Always 0
  */
-
 void _print_rev_recursion(char *s)
 {
-if (*s)
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
